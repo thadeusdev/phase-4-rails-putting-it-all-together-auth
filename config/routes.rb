b6e1ea360dc23236b95070eb1abfb0fe16976662
thadeus <thadeusnyariki94@gmail.com>
2022-12-02 13:32:26 +0300

    Done assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # Recipes view and create
+    resources :recipes, only: [:index, :create]
+    # Sign-up feature
+    post "/signup", to: "users#create"
+    # Auto-Login feature
+    get "/me", to: "users#show"
+    # Login after sign-up
+    post "/login", to: "sessions#create"
+    # Logout after login
+    delete "/logout", to: "sessions#destroy"
 end
